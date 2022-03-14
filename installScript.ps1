491eb764a740f8a993dc34cb430640dcfc020179
Alex Sink <alexsink@yahoo.com>
2022-03-14 14:01:14 -0700

    chmod wasn't needed

--- installScript.ps1
+++ installScript.ps1
@@ -3,15 +3,12 @@ adb install -r -g -d "Builds/test.apk"
 Write-Host "installed apk"
 
 Write-Host "double checking that root directories exist"
-adb shell mkdir -m777 /mnt/sdcard/Android/data/com.DefaultCompany.AccessStorage/files
+adb shell mkdir /mnt/sdcard/Android/data/com.DefaultCompany.AccessStorage/files
 
 "fakefilecontents" | Out-file testfile
 
 echo "pushing sample folder of files"
 adb push testfile /mnt/sdcard/Android/data/com.DefaultCompany.AccessStorage/files/testfile
-
-echo "double checking permissions of files"
-adb shell chmod -R 777 /mnt/sdcard/Android/data/com.DefaultCompany.AccessStorage/files
 
 echo "folder of content files should now be located and accessible at /mnt/sdcard/Android/data/com.DefaultCompany.AccessStorage/files"
 
