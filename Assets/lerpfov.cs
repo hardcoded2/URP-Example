c00511d42e5eeda7d0a4820cc66b9910ae6c6743
Alex Sink <alexsink@yahoo.com>
2022-06-06 12:28:37 -0700

    fov and other assets

---
+++ Assets/lerpfov.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpfov : MonoBehaviour
+{
+    Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _camera.fieldOfView = 40 + (Time.frameCount % 40);
+    }
+}
